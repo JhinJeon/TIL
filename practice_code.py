757a51fc3fcb098060dcd4434ed5d663d7449749
JhinJeon <jdznawa@naver.com>
2022-07-28 17:54:35 +0900

    0728 수업내용 정리

---
+++ practice_code.py
@@ -0,0 +1,24 @@
+class Person:
+    counts = 0
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def call_name(self):
+        return f'대전 2반 {self.name} 입니다!'
+
+    @staticmethod
+    def hello():
+        return '안녕하세요!'
+
+
+class Student(Person):
+
+    def call_name(self):
+        return f'대전 2반 {self.name} 입니다!'
+
+
+person1 = Person("김성준", 25)
+student1 = Student("박승재", 25)
+print(student1.call_name())
